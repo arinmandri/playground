cd25814ee7fd6db4cb2863733d608a8810004a6b
Evergood <arinmandri@gmail.com>
2025-07-29 17:55:47 +0900

    이미 로그인한 상태이면 로그인화면에서 홈으로

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -3,17 +3,30 @@ import Home from "../views/HomeView.vue";
 import Board from "../views/BoardView.vue";
 import MemberJoin from "../views/member/JoinView.vue";
 import MemberLogin from "../views/member/LoginView.vue";
+import { useAuthStore } from "@/stores/auth";
 
 const routes = [
-  { path: "/", component: Home },
-  { path: "/board", component: Board },
-  { path: "/member/join", component: MemberJoin },
-  { path: "/member/login", component: MemberLogin },
+  { name: "home", path: "/", component: Home },
+  { name: "board", path: "/board", component: Board },
+  { name: "memberJoin", path: "/member/join", component: MemberJoin },
+  { name: "memberLogin", path: "/member/login", component: MemberLogin },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+// 전역 가드
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore();
+
+  //// 이미 로그인 상태이면 홈으로
+  if (to.name === "memberLogin" && authStore.isAuthenticated) {
+    return next({ name: "home" });
+  }
+
+  return next();
 });
 
 export default router;
