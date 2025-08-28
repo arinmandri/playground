5e16be4faa52fdb9e1b1ce1043ab68925fc756b9
Evergood <arinmandri@gmail.com>
2025-08-28 15:42:46 +0900

    (메모)

--- front/src/service/boardService.ts
+++ front/src/service/boardService.ts
@@ -3,6 +3,11 @@ import type { Post, PostRaw } from "@/types/board";
 import type { SimpleListPack } from "@/types/index";
 import api from "@/api/axiosInstance";
 
+/* XXX
+- 아주 많이 가져온 다음엔; 오래된 항목은 삭제하고; 다음페이지 말고 이전페이지 더보기가 있어야?
+- 페이지당 항목 수는 백이 아니라 프론트에? 백에선 페이지 크기 필수로 받고?
+*/
+
 //// 게시판 데이터 가져오기
 export async function fetchPostList(listPack:SimpleListPack<Post>): Promise<void> {
   try {
