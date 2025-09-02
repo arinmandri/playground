6777d58cd8f6e22b1a9877d59c5508e9ae4e94ba
Evergood <arinmandri@gmail.com>
2025-09-02 13:14:13 +0900

    AWS S3 업로드 기능을 FileSer로 옮김.

--- back/src/main/java/xyz/arinmandri/playground/api/ApiFile.java
+++ back/src/main/java/xyz/arinmandri/playground/api/ApiFile.java
@@ -1,7 +1,7 @@
 package xyz.arinmandri.playground.api;
 
 import java.io.File;
-import java.util.concurrent.CompletableFuture;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -14,9 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import lombok.RequiredArgsConstructor;
-import software.amazon.awssdk.services.s3.model.PutObjectResponse;
-import software.amazon.awssdk.services.s3.model.S3Exception;
-import xyz.arinmandri.playground.aws.S3Actions;
 import xyz.arinmandri.playground.core.file.FileSer;
 
 
@@ -28,7 +25,6 @@ public class ApiFile
 	private static final Logger logger = LoggerFactory.getLogger( ApiFile.class );
 
 	final FileSer fileSer;
-	static S3Actions s3Actions = new S3Actions();// TODO static 말고 빈으로 바꾸기
 
 	@PostMapping( "/simple/add" )
 	public ResponseEntity<String> apiFileSimpleAdd (
@@ -43,25 +39,7 @@ public class ApiFile
 			        .body( "File upload failed" );
 		}
 
-		// TODO
-		String bucketName = "arinmandri";
-		String key = "testKey";
-		String objectPath = file1.getAbsolutePath();
-		try{
-			CompletableFuture<PutObjectResponse> future = s3Actions.uploadLocalFileAsync( bucketName, key, objectPath );
-			future.join();
-
-		}
-		catch( RuntimeException rt ){
-			Throwable cause = rt.getCause();
-			if( cause instanceof S3Exception s3Ex ){
-				logger.info( "S3 error occurred: Error message: {}, Error code {}", s3Ex.getMessage(), s3Ex.awsErrorDetails().errorCode() );
-			}
-			else{
-				logger.info( "An unexpected error occurred: " + rt.getMessage() );
-			}
-			// throw cause;
-		}
+		fileSer.uploadTest( file1 );
 
 		return ResponseEntity.status( HttpStatus.CREATED )
 		        .body( "File upload is not implemented yet" );
--- back/src/main/java/xyz/arinmandri/playground/core/file/FileSer.java
+++ back/src/main/java/xyz/arinmandri/playground/core/file/FileSer.java
@@ -5,11 +5,18 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.SecureRandom;
 import java.util.Base64;
+import java.util.concurrent.CompletableFuture;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import software.amazon.awssdk.services.s3.model.PutObjectResponse;
+import software.amazon.awssdk.services.s3.model.S3Exception;
+import xyz.arinmandri.playground.aws.S3Actions;
 
 
 /*
@@ -22,28 +29,56 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 public class FileSer
 {
+	private static final Logger logger = LoggerFactory.getLogger( FileSer.class );
+
 	private final int tempFileNameLength = 24;
 
-	private final String tempDirBase;
+	final private String tempDirBase;
 	final private String[] currentBucketNames = { "bucket1", "bucket2" };
 	private int currentBucketIndex = 0;
+	final private String awsBucketName;
 
 	final SecureRandom random;
 
+	static S3Actions s3Actions = new S3Actions();// TODO static 말고 빈으로 바꾸기
+
 	public FileSer(
 	        @Value( "${file.temp-directory-base}" ) String tempDirBase ,
+	        @Value( "${aws.s3.bucket-name}" ) String awsBucketName ,
 	        @Autowired SecureRandom random ) {
 
 		this.tempDirBase = tempDirBase;
+		this.awsBucketName = awsBucketName;
 		this.random = random;
 
 		/*
 		 * 앱 시작 시 버킷 폴더가 없으면 생성
 		 */
-		for( String bucketName : currentBucketNames ){
-			File folder = new File( tempDirBase + "/" + bucketName );
+		for( String localBucketName : currentBucketNames ){
+			File folder = new File( tempDirBase + "/" + localBucketName );
 			if( !folder.exists() )
 			    folder.mkdirs();
+		}
+	}
+
+	public void uploadTest ( File file ) {
+
+		String key = file.getName();
+		String objectPath = file.getAbsolutePath();
+		try{
+			CompletableFuture<PutObjectResponse> future = s3Actions.uploadLocalFileAsync( awsBucketName, key, objectPath );
+			future.join();
+
+		}
+		catch( RuntimeException rt ){
+			Throwable cause = rt.getCause();
+			if( cause instanceof S3Exception s3Ex ){
+				logger.info( "S3 error occurred: Error message: {}, Error code {}", s3Ex.getMessage(), s3Ex.awsErrorDetails().errorCode() );
+			}
+			else{
+				logger.info( "An unexpected error occurred: " + rt.getMessage() );
+			}
+			// throw cause;
 		}
 	}
 
@@ -54,7 +89,7 @@ public class FileSer
 		if( multipartFile == null || multipartFile.isEmpty() )
 		    return null;
 
-		final Path path = Paths.get( generateRandomLocalFilePath() );
+		final Path path = Paths.get( generateRandomLocalFilePath() + "." + getExtension( multipartFile.getOriginalFilename() ) );
 
 		File file;
 		try{
@@ -86,6 +121,21 @@ public class FileSer
 
 	private String getTempBucketDir () {
 		return getTempBucketDir( currentBucketIndex );
+	}
+
+	/**
+	 * 파일 이름에서 확장자 추출
+	 * 
+	 * @param fileName
+	 * @return 확장자(소문자), 없으면 빈 문자열
+	 */
+	public String getExtension ( String fileName ) {
+
+		int li = fileName.lastIndexOf( "." );
+		if( li < 0 || li >= fileName.length() - 1 ){
+			return "";
+		}
+		return fileName.substring( li + 1, fileName.length() ).toLowerCase();
 	}
 
 	/**
