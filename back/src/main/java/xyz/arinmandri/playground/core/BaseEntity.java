f905022196d38e67768856f13cab0179263d10de
Evergood <arinmandri@gmail.com>
2025-09-10 16:45:17 +0900

    BaseEntity.equals 수정: Long ==

--- back/src/main/java/xyz/arinmandri/playground/core/BaseEntity.java
+++ back/src/main/java/xyz/arinmandri/playground/core/BaseEntity.java
@@ -2,15 +2,14 @@ package xyz.arinmandri.playground.core;
 
 import java.time.Instant;
 
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import lombok.Getter;
 
 @EntityListeners( AuditingEntityListener.class )
@@ -30,8 +29,8 @@ public abstract class BaseEntity
 	@Override
 	public boolean equals ( Object other ) {
 		if( other == null ) return false;
-		if( other instanceof BaseEntity ){
-			return id == ( (BaseEntity) other ).id;
+		if( other instanceof BaseEntity baseEntity ){
+			return id.equals( baseEntity.id );
 		}
 		return false;
 	}
