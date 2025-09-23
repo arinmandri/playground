e4edbef298918bb63a0384283a4efb6e9e7c28d4
Evergood <arinmandri@gmail.com>
2025-09-23 20:53:40 +0900

    게시물 1개 컴포넌트 분리

--- front/src/views/board/View.vue
+++ front/src/views/board/View.vue
@@ -2,21 +2,8 @@
   <router-link to="/board/post/write"><button>글쓰기</button></router-link>
   <div v-if="postListPack.list.length > 0">
     <div class="posts">
-      <div class="post" v-for="post in postListPack.list" :key="post.id">
-        <p class="content">{{ post.content }}</p>
-        <div>
-          <template v-for="attachment in post.attachments">
-            <Y_PAttachmentComp :attachment="attachment"></Y_PAttachmentComp>
-          </template>
-        </div>
-        <hr />
-        <div class="author">
-          <span>{{ post.author.nick }}</span>
-        </div>
-        <p class="info">
-          <span>{{ post.createdAtPretty }}</span>
-        </p>
-      </div>
+      <Y_PostListItemComp v-for="post in postListPack.list" :key="post.id" :post="post">
+      </Y_PostListItemComp>
     </div>
     <div class="listEnd">
       <button v-if="!postListPack.isEnd" @click="clickMoreBtn">더보기</button>
@@ -26,7 +13,8 @@
 </template>
 
 <script setup lang="ts">
-import Y_PAttachmentComp from "@/views/board/post/comp/Y_PAttachmentComp.vue";
+
+import Y_PostListItemComp from "./post/comp/Y_PostListItemComp.vue";
 
 import type { Post } from "@/views/board/post/types";
 import type { SimpleListPack } from "@/types/index";
@@ -54,49 +42,9 @@ function clickMoreBtn() {
 .posts {
   display: flex;
   flex-direction: column;
-  gap: 10px;
+  gap: 15px;
 }
 
-.post {
-  border: 1px solid #e0e0e0;
-  background: #fafbfc;
-  padding: 16px 16px 2px 16px;
-  border-radius: 8px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.04);
-  transition: box-shadow 0.2s;
-}
-
-.post:hover {
-  box-shadow: 0 4px 16px rgba(0, 0, 0, 0.08);
-}
-
-.post .content {
-  font-size: 1.1rem;
-  margin-bottom: 8px;
-  color: #333;
-}
-
-.post hr {
-  border: 2px solid #8882;
-  margin: 18px 4px;
-}
-.post .author {
-  font-size: 0.95rem;
-  color: var(--point-blue);
-  margin-bottom: 4px;
-}
-
-.post .info {
-  margin-top: 6px;
-  font-size: 0.85rem;
-  color: #888;
-  text-align: right;
-}
-
-.attachment-image {
-  max-width: 200px;
-  max-height: 200px;
-}
 .listEnd {
   text-align: center;
   margin: 20px 0;
---
+++ front/src/views/board/post/comp/Y_PostListItemComp.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="post">
+    <div>
+      <template v-for="attachment in props.post.attachments">
+        <Y_PAttachmentComp :attachment="attachment">
+        </Y_PAttachmentComp>
+      </template>
+    </div>
+    <p class="content">{{ props.post.content }}</p>
+    <hr />
+    <div class="author">
+      <span>{{ props.post.author.nick }}</span>
+    </div>
+    <p class="info">
+      <span>{{ props.post.createdAtPretty }}</span>
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import Y_PAttachmentComp from "@/views/board/post/comp/Y_PAttachmentComp.vue";
+
+import { type Post } from "@/views/board/post/types";
+
+const props = defineProps<{
+  post: Post;
+}>();
+
+</script>
+
+<style scoped>
+
+.post {
+  border: 1px solid #e0e0e0;
+  background: #fafbfc;
+  padding: 16px 16px 2px 16px;
+  border-radius: 8px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.04);
+  transition: box-shadow 0.3s;
+}
+
+.post:hover {
+  box-shadow: 0 1px 20px #0002;
+}
+
+.post .content {
+  font-size: 1.1rem;
+  margin-bottom: 8px;
+  color: #333;
+}
+
+.post hr {
+  border-top: 2px solid #8882;
+  margin: 18px 4px;
+}
+
+.post .author {
+  font-size: 0.95rem;
+  color: var(--point-blue);
+  margin-bottom: 4px;
+}
+
+.post .info {
+  margin-top: 6px;
+  font-size: 0.85rem;
+  color: #888;
+  text-align: right;
+}
+
+.attachment-image {
+  max-width: 200px;
+  max-height: 200px;
+}
+.listEnd {
+  text-align: center;
+  margin: 20px 0;
+}
+</style>
