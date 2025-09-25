3d790571f14327133a74e4964cb20fcd3a6c1039
Evergood <arinmandri@gmail.com>
2025-09-25 10:09:39 +0900

    (내부적) 이름 바꿈

--- front/src/views/board/post/write/PostWriteForm.vue
+++ front/src/views/board/post/write/PostWriteForm.vue
@@ -1,7 +1,7 @@
 <template>
 	<form @submit.prevent="submitPost">
 		<textarea v-model="formData.content" type="text">뭐 쓸라고 했더라</textarea>
-		<PAttachmentListForm ref="attachmentsComp" :title="'첨부파일'"
+		<PAttachmentListForm ref="pAttachmentListForm" :title="'첨부파일'"
 			v-model:attachments="(formData.attachments as PAttachment[])" :maxLength="5" />
 		<button type="submit">라고 쓰기</button>
 	</form>
@@ -14,32 +14,32 @@ import PAttachmentListForm from '@/views/board/post/comp/PAttachmentListForm.vue
 import { type PostWrite, PAttachment } from "@/views/board/services/types";
 import { type Z_PostAdd, toApiSendingFormOfAtt } from "@/api/board";
 
-import { ref, type Ref } from 'vue'
+import { ref, type Ref, defineExpose } from 'vue'
 
 const emit = defineEmits<{
 	(e: 'submit', exportProps: Z_PostAdd): void;
 }>();
-
-interface PAttachmentList {
-	uploadFiles: () => Promise<void>
-}
-
-const attachmentsComp = ref<PAttachmentList>() as Ref<PAttachmentList>;
 
 const formData = ref<PostWrite>({
 	content: '',
 	attachments: [],
 }) as Ref<PostWrite>;
 
+interface PAttachmentListForm_intf {
+	uploadFiles: () => Promise<void>
+}
+const pAttachmentListForm = ref<PAttachmentListForm_intf>() as Ref<PAttachmentListForm_intf>;
+
 
 async function submitPost() {
-	await attachmentsComp.value.uploadFiles();
+	await pAttachmentListForm.value.uploadFiles();
 	const attsToSend = formData.value.attachments.map(attRaw => toApiSendingFormOfAtt(attRaw));
 	emit('submit', {
 		content: formData.value.content,
 		attachments: attsToSend,
 	});
 	formData.value.content = ''
+	formData.value.attachments = [];
 }
 
 // TODO loading?
